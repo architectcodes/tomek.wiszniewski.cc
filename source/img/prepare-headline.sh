372ad1af09038b916c73552913699ae114bbe412
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-03 10:02:01 +0200

    Add `prepare-headline.sh`

---
+++ source/img/prepare-headline.sh
@@ -0,0 +1,36 @@
+#! /bin/sh -e
+
+if test "$1" = '--help' -o -z "$1"; then echo -n "\
+Usage:
+  $0 ⟪basename⟫
+
+We’ll take ⟪basename⟫.ready.png and generate three files out of it:
+
+  • ⟪basename⟫.jpg
+  • ⟪basename⟫@2x.jpg
+  • ⟪basename⟫@preview.png
+
+Dependencies:
+
+  • ImageMagick
+  • ./noise.tif
+  • ./noise@2x.tif
+"; exit; fi
+
+set -x
+
+convert "$1.ready.png" 'noise.tif' \
+  -resize 1920 \
+  -composite \
+  -quality 92 \
+  "$1.jpg"
+
+convert "$1.ready.png" 'noise@2x.tif' \
+  -resize 3840 \
+  -composite \
+  -quality 92 \
+  "$1@2x.jpg"
+
+convert "$1.jpg" \
+  -resize 12x3 \
+  "$1@preview.png"
